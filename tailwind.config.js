478f586fe8d6f95e666a21336bd1067e17ec89aa
usama0011 <za5232208@gmail.com>
2021-10-22 11:43:45 +0500

    new build

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'primary-color': "#2074BB"
+      },
+      fontFamily: {
+        'Snel-BT': "'Snel BT', serif",
+        'Roboto': "Roboto, serif"
+      },
+      screens:{
+        'laptop':'1010px'
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
